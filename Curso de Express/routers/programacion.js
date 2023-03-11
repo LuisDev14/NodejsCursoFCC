83e6fcb8dc5a4e2c8be6ae4951e898d43b175364
Luis Garcia <gbluisman@gmail.com>
2023-03-10 21:37:13 -0600

    [ADD] se agregaron los metodos put patch y delete de HTTP

--- Curso de Express/routers/programacion.js
+++ Curso de Express/routers/programacion.js
@@ -4,6 +4,10 @@ const express = require('express');
 const {programacion} = require('../datos/cursos.js').infoCursos;
 
 const routerProgramacion = express.Router();
+
+//Middleware
+routerProgramacion.use(express.json)
+
 //Programacion
 routerProgramacion.get('/',(req,res)=>{
   res.send(JSON.stringify(programacion));
@@ -34,5 +38,47 @@ routerProgramacion.get('/:lenguaje/:nivel',(req,res)=>{
 
   res.send(JSON.stringify(resultados));
 });
+
+//post
+routerProgramacion.post('/',(req,res)=>{
+  let cursoNuevo = req.body;
+  programacion.push(cursoNuevo);
+  res.send(JSON.stringify(programacion))
+});
+
+//put
+routerProgramacion.put('/:id',(req,res)=>{
+  const cursoActualizado = req.body;
+  const id = req.params.id;
+
+  //obtener el indice del arreglo de cursos
+  const indice =  programacion.findIndex(curso=>curso.id == id);
+  if(indice>=0){
+    programacion[indice]= cursoActualizado;
+  }
+  res.send(JSON.stringify(programacion));
+});
+
+routerProgramacion.patch('/:id',(req,res)=>{
+  const infoActualizada = req.body;
+  const id  =  req.params.id;
+
+  const indice =  programacion.findIndex(curso=>curso.id == id );
+  if(indice >=0){
+    const cursoAModificar = programacion[indice];
+    Object.assign(cursoAModificar,infoActualizada);
+  }
+  res.send(JSON.stringify(programacion));
+});
+
+//Delete
+routerProgramacion.delete('/:id',(req,res)=>{
+  const id = req.params.id;
+  const indice = programacion.findIndex(curso=>curso.id==id)
+  if(indice>=0){
+    programacion.splice(indice,1);
+  }
+  res.send(JSON.stringify(programacion))
+})
 
 module.exports = routerProgramacion;
